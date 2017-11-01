08c60b15ee8e904e6d26dc7d0c86ff6791aa3320
ffuis-aljosa <a.sljuka@ffuis.edu.ba>
2017-11-01 13:23:19 +0100

    Reading Movies from DB

--- MovieShop/MoviesForm.cs
+++ MovieShop/MoviesForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlServerCe;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,35 @@ namespace MovieShop
 {
     public partial class MoviesForm : Form
     {
+        SqlCeConnection dbConnection;
+        string connectionString = @"Data Source=..\..\MovieShop.sdf;Password=StrongPassword";
+
         public MoviesForm()
         {
+            dbConnection = new SqlCeConnection(connectionString);
+            dbConnection.Open();
+
             InitializeComponent();
+
+            loadMovies();
+        }
+
+        private void loadMovies()
+        {
+            SqlCeCommand command = new SqlCeCommand("SELECT Title, Year, Genre FROM Movies", dbConnection);
+            SqlCeDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string title = reader.GetString(0);
+                int year = reader.GetInt32(1);
+                string genre = reader.GetString(2);
+
+                Console.WriteLine(title + year + genre);
+
+                Movie newMovie = new Movie(title, year, genre);
+                moviesListBox.Items.Add(newMovie);
+            }
         }
 
         private void addMovieButton_Click(object sender, EventArgs e)
